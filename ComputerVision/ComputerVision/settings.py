0520d4d28509ee64aa0256878f593c8ade311a78
Miszo <kol478@gmail.com>
2020-01-17 00:25:47 +0100

    Register number_recognition app in project settings.

--- ComputerVision/ComputerVision/settings.py
+++ ComputerVision/ComputerVision/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'number_recognition.apps.NumberRecognitionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
