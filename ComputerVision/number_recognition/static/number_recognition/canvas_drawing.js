a33e68811eb51a51c01709cc940b7130fbaf44e1
Miszo <kol478@gmail.com>
2020-01-17 00:27:23 +0100

    Add canvas to draw numbers.

---
+++ ComputerVision/number_recognition/static/number_recognition/canvas_drawing.js
@@ -0,0 +1,83 @@
+canvas = document.getElementById('myCanvas');
+context = canvas.getContext("2d");
+
+clickX = [];
+clickY = [];
+clickDrag = [];
+let paint;
+
+function addClick(x, y, dragging) {
+
+    clickX.push(x);
+    clickY.push(y);
+    clickDrag.push(dragging);
+}
+
+function redraw() {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+
+    context.strokeStyle = "#df4b26";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+
+    for (var i = 0; i < clickX.length; i++) {
+
+        context.beginPath();
+        if (clickDrag[i] && i) {
+            context.moveTo(clickX[i - 1], clickY[i - 1]);
+        } else {
+            context.moveTo(clickX[i] - 1, clickY[i]);
+        }
+        context.lineTo(clickX[i], clickY[i]);
+        context.closePath();
+        context.stroke();
+    }
+
+
+}
+
+function onmousedown(e) {
+
+    paint = true;
+    addClick(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
+    redraw();
+}
+
+function onmousemove(e) {
+    if (paint) {
+        addClick(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop, true);
+        redraw();
+    }
+}
+
+function onmouseup(e) {
+    paint = false;
+}
+
+function onmouseleave(e) {
+    paint = false;
+}
+
+canvas.onmousedown = function (e) {
+    onmousedown(e)
+};
+
+canvas.onmouseup = function (e) {
+    onmouseup(e)
+};
+
+canvas.onmousemove = function (e) {
+    onmousemove(e)
+};
+
+canvas.onmouseleave = function (e) {
+    onmouseleave(e)
+};
+
+
+function clearCanvas() {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+    clickX = [];
+    clickY = [];
+    clickDrag = [];
+}
--- ComputerVision/number_recognition/views.py
+++ ComputerVision/number_recognition/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the number recognition app index.")
+    return render(request, 'number_recognition/index.html')
