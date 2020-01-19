2193119bd3b9ced34169f39a2f74d4545b6d0eed
Miszo <kol478@gmail.com>
2020-01-19 23:16:04 +0100

    Draw image, type what it is and send with form.

--- ComputerVision/number_recognition/urls.py
+++ ComputerVision/number_recognition/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'number_recognition'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('send_drawing', views.send_drawing, name='send_drawing'),
 ]
--- ComputerVision/number_recognition/views.py
+++ ComputerVision/number_recognition/views.py
@@ -8,3 +8,8 @@ from django.shortcuts import render, get_object_or_404
 
 def index(request):
     return render(request, 'number_recognition/index.html')
+
+
+def send_drawing(request):
+    return HttpResponse(
+        "typed_number: {} \n\n image: \n\n {}".format(request.POST['typed_number'], request.POST['canvas']))
