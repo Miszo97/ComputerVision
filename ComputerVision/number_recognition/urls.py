98c4bf478ed02328eb6569286f6c1a06cbab7a86
Miszo <kol478@gmail.com>
2020-01-22 14:22:34 +0100

    Add special page to revise submitted examples.

--- ComputerVision/number_recognition/urls.py
+++ ComputerVision/number_recognition/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('feed_model', views.feed_model, name='feed_model'),
     path('send_drawing', views.send_drawing, name='send_drawing'),
+    path('examples_selection', views.examples_selection, name='examples_selection'),
 ]
--- ComputerVision/number_recognition/views.py
+++ ComputerVision/number_recognition/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-
 # Create your views here.
 
-from django.http import HttpResponse, HttpResponseRedirect
+from PIL import Image
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+import numpy as np
+import io
+import re
+import base64
 
 from .models import User_Example
 
@@ -18,6 +21,20 @@ def feed_model(request):
 
 
 def send_drawing(request):
+    image_b64 = request.POST['canvas']
+    # with io.StringIO(image_b64) as f:
+    #     print(f.read(100))
+    imgstr = re.search(r'base64,(.*)', image_b64).group(1)
+    image_bytes = io.BytesIO(base64.b64decode(imgstr))
+    im = Image.open(image_bytes)
+    arr = np.array(im)[:, :, 0]
+
     example = User_Example(drawing_base64=request.POST['canvas'], label=request.POST['typed_number'])
     example.save()
     return HttpResponseRedirect(reverse('number_recognition:feed_model'))
+
+
+def examples_selection(request):
+    examples = User_Example.objects.all()
+
+    return render(request, 'number_recognition/examples_selections.html', context={"examples": examples})
