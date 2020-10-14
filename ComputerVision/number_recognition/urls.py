0ab42869695da0a7278213866bfd3d62e3869c14
Miszo <kol478@gmail.com>
2020-10-14 17:10:05 +0200

    Add new url for model request

--- ComputerVision/number_recognition/urls.py
+++ ComputerVision/number_recognition/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('feed_model', views.feed_model, name='feed_model'),
     path('predict_number', views.predict_number, name='predict_number'),
     path('send_drawing', views.send_drawings, name='send_drawings'),
+    path('request_a_model', views.request_a_model, name='request_a_model'),
     path('examples_selection', views.examples_selection, name='examples_selection'),
     path('update_examples', views.update_examples, name='update_examples'),
 
