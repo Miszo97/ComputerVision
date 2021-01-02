5720e4de4b451db4ea38eb894b756e01711e2a6a
Miszo97 <kol478@gmail.com>
2021-01-02 16:43:24 +0100

    procfile update

--- number_recognition_frontend/views.py
+++ number_recognition_frontend/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+# Create your views here.   
 
-
+@csrf_exempt
 def index(request, *args, **kwargs):
     return render(request, 'number_recognition_frontend/index.html')
