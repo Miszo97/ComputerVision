14d202e42067dcb0e265e5284b8cda844a197588
Miszo97 <kol478@gmail.com>
2021-01-03 21:17:53 +0100

    get rid of django_heroku

--- ComputerVision/settings.py
+++ ComputerVision/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_on_heroku
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,6 +124,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Activate Django-Heroku.
-django_on_heroku.settings(locals())
